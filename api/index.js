66fbdf3fa4a74d3b8d8429e5bed0909ed27ff26f
shivarajkulal <shivarajkulalsn884@gmail.com>
2024-03-23 21:13:02 +0530

    jwt password authantication

--- api/index.js
+++ api/index.js
@@ -1,20 +1,55 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const User = require("./models/User.js");
 const app = express();
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000' }));
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://shivarajkulalsn884:Z8A1JzEZViRfCv7Q@cluster0.bfndkuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-);
+const secret = "ejskEHSJDghja12ajejWJEJSq2eie";
 
+// mongoose.connect(
+//   "mongodb+srv://shivarajkulalsn884:Z8A1JzEZViRfCv7Q@cluster0.bfndkuf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+// );
+
+mongoose.connect("mongodb://localhost:27017/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Z8A1JzEZViRfCv7Q
+
+//request from RegisterPage.js
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const userDoc = await User.create({username, password}); 
-  res.json(userDoc);
+  const saltRounds = 10;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const userDoc = await User.create({
+      username,
+      password: hashedPassword,
+    });
+    res.json(userDoc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = await bcrypt.compare(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
+  }
 });
 
 const PORT = 5000;
